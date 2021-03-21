ab3fe9a9f72517049e68965019181e2b48527628
Rafał Kogut <sefir@o2.pl>
2021-03-21 18:46:24 +0100

    dodanie eslint

--- src/server/getExamplesPaths.js
+++ src/server/getExamplesPaths.js
@@ -1,22 +1,23 @@
 const fs = require('fs');
 const path = require('path');
 
-function getExamplesPaths(){
+function getExamplesPaths() {
   const clientPath = path.join(process.cwd(), 'src/client');
-  const isDirectory = source => fs.lstatSync(source).isDirectory()
-  const getDirectories = source =>
-    fs.readdirSync(source).map(name => path.join(source, name)).filter(isDirectory)
+  const isDirectory = source => fs.lstatSync(source).isDirectory();
+  const getDirectories = source => fs.readdirSync(source)
+    .map(name => path.join(source, name))
+    .filter(isDirectory);
   const directories = getDirectories(clientPath);
-  const directoriesHasIndexHTML = directories.map((directory)=>{
+  const directoriesHasIndexHTML = directories.map((directory) => {
     const indexHtml = path.join(directory, 'index.html');
-    if(fs.existsSync(indexHtml)) {
+    if (fs.existsSync(indexHtml)) {
       return {
-        dirName:path.basename(directory),
-      }
+        dirName: path.basename(directory),
+      };
     }
     return null;
-  })
-  return directoriesHasIndexHTML.filter(directory=>directory!==null);
+  });
+  return directoriesHasIndexHTML.filter(directory => directory !== null);
 }
 
 module.exports = getExamplesPaths;
--- src/server/index.js
+++ src/server/index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+
 const app = express();
 const http = require('http').Server(app);
 const io = require('socket.io')(http);
@@ -7,18 +8,18 @@ const getExamplesPaths = require('./getExamplesPaths');
 
 
 app.use(express.static(path.join(process.cwd(), './src/public')));
-getExamplesPaths().forEach(examplePath=>{
-  app.use('/'+examplePath.dirName, express.static(path.join(process.cwd(), 'src/client', examplePath.dirName)))
-})
+getExamplesPaths().forEach((examplePath) => {
+  app.use(`/${examplePath.dirName}`, express.static(path.join(process.cwd(), 'src/client', examplePath.dirName)));
+});
 app.get('/', (req, res) => {
   res.sendFile(path.join(process.cwd(), 'src', 'public', 'index.html'));
 });
 
 io.on('connection', (socket) => {
   console.log('a user connected');
-  socket.on('client-get-examples', ()=>{
+  socket.on('client-get-examples', () => {
     socket.emit('client-get-examples', getExamplesPaths());
-  })
+  });
 });
 
 http.listen(3000, () => {
